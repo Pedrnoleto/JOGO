e43d18b22aeca98d758ae6ffac9e58e485ce9826
Pedrnoleto <149097151+Pedrnoleto@users.noreply.github.com>
2024-06-21 23:03:17 -0300

    Atualiação

--- src/conexao/conexao.java
+++ src/conexao/conexao.java
@@ -5,29 +5,75 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexao {
-    
+
     private static final String url = "jdbc:mysql://localhost:3306/usuarios";
     private static final String user = "Root";
     private static final String passorwd = "1234";
 
-
-    private static Connection conn;
-
-    public static Connection getConexao(){
-
-        try{
-        if (conn = null){
-            conn = DriverManager.getConnection(url, user, passorwd);
-            return conn;
-
-        } else {
-            return conn;
-        }
-        }catch (SQLException e) {
-            // TODO: handle exception
+    private ConexaoBD() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado com sucesso");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Falha na conexão!");
             e.printStackTrace();
-            return null;
         }
-    
-}
+    }
+
+    public static ConexaoBD getInstance() {
+        if (instance == null) {
+            instance = new ConexaoBD();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void executarSQL(String sql) { // caso queira executar um comando
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            System.out.println("Comando SQL executado com sucesso: " + sql);
+        } catch (SQLException e) {
+            System.out.println("Erro ao executar o comando SQL: " + sql);
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public ResultSet executarConsulta(String sql) { // caso queira executar uma cosulta ao banco de dados
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            System.out.println("Erro ao executar a consulta SQL: " + sql);
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public void closeConDB() {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Conexão fechada com sucesso!");
+            } catch (SQLException e) {
+                System.out.println("Erro ao fechar a conexão!");
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/dao/Userdao.java
+++ src/dao/Userdao.java
@@ -1,28 +1,60 @@
+package JOGO;
+
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import entity.Usuario;
 
-public class Userdao {
+public class UserDao {
+    ConexaoBD conexao = ConexaoBD.getInstance();
 
-    public void cadastrasUsuario(Usuario Uuario){
+    public void cadastarUsuario(int idade, String login, String senha) {
+        String sql = "INSERT INTO usuarios (nome, idade, cidade, login, senha) VALUES ('" + login + ", '" + idade
+                + "', '" + senha + "');";
+        conexao.executarSQL(sql);
+    }
 
-        String sql = "INSERT INTO USUARIO (LOGIN, SENHA, IDADE) VALUES (?, ?, ?)";
+    public boolean login(String login, String senha) throws SQLException {
+        String sql = "SELECT login, senha FROM usuarios WHERE login = '" + login + "' and senha = '" + senha + "';";
+        ResultSet resultado = null;
+        resultado = conexao.executarConsulta(sql);
+        if (resultado.next()) {
+            return true;
+        } else {
+            return false;
+        }
 
-        PreparedStatement ps = null;
+    }
+
+    public ArrayList<String> dadosUsuario(String login) {
+        String sql = "SELECT id, idade FROM usuarios WHERE login = '" + login + "';";
+        ResultSet result = null;
+        int id = 0;
+        int idade = 0;
 
         try {
-            ps = Conexao.getConexao().PreparedStatement(sql);
-            ps.setString(1, usuario.getLogin());
-            ps.setString(2, usuario.getSenha());
-            ps.setInt(3, usuario.getIdade());
+            result = conexao.executarConsulta(sql);
+            if (result.next()) {
+                try {
+                    id = result.getInt("id");
+                    idade = result.getInt("idade");
 
-
-            ps.execute();
-            ps.close();
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
+                } catch (Exception e) {
+                    System.out.println("Erro" + e.getMessage());
+                }
+            } else {
+                System.out.println("erro");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro " + e.getMessage());
         }
+        ArrayList<String> resultado = new ArrayList<>();
+        resultado.add(Integer.toString(id));
+        resultado.add(Integer.toString(idade));
+
+        return resultado;
     }
+
 }
